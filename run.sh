e90139c7a5e61053a1045719d398c33b3f278ea6
Bukowa <gitbukowa@gmail.com>
2024-06-15 20:02:56 +0200

    feat: add first beta feature
    Signed-off-by: Bukowa <gitbukowa@gmail.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+# Create new git repository in temporary directory
+DIR=$(mktemp -d)
+cp cliff.toml $DIR
+cp expected.md $DIR
+
+cd $DIR
+git init .
+
 GIT_COMMITTER_DATE="2021-01-23 01:23:45" git commit --allow-empty -m "feat: add first beta feature"
 git tag v1.0.0-beta.1
 
@@ -13,3 +21,7 @@ git tag v1.0.0
 
 GIT_COMMITTER_DATE="2021-01-23 01:23:49" git commit --allow-empty -m "fix: simple fix"
 git tag v1.0.1
+
+diff --side-by-side --color \
+  <(git-cliff) \
+  expected.md
